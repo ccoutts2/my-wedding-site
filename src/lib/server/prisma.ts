92bafea5c3534dd565e6c3889d7bfd41dd4cf29e
Chris Coutts <ccoutts@mmgy.com>
2026-04-08 11:34:40 +0100

    Revert prisma.ts file

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,5 +1,20 @@
 import { PrismaClient } from '../../generated/prisma/client';
+import { DATABASE_URL } from '$env/static/private';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const globalForPrisma = globalThis as unknown as {
+	prisma: any;
+};
+
+export const prisma =
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		datasources: {
+			db: {
+				url: DATABASE_URL
+			}
+		}
+	});
+
+globalForPrisma.prisma = prisma;
+
+export default prisma;
