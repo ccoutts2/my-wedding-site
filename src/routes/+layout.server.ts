de6daee7bca755db740c28cd22ca311bd8c1e5e9
Chris Coutts <ccoutts@mmgy.com>
2026-04-14 15:39:15 +0100

    Re-introduce neon connection string server logic

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,17 @@
+import { DATABASE_URL } from '$env/static/private';
+import { neon } from '@neondatabase/serverless';
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+const connectionString: string = DATABASE_URL as string;
+const sql = neon(connectionString);
+
+export async function load() {
+	const response = await sql`SELECT version()`;
+	const { version } = response[0];
+	return {
+		version
+	};
+}
 
 injectAnalytics({ mode: dev ? 'development' : 'production' });
